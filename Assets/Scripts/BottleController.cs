1c6c5482311152f00ec4eafd15a005fafabacc01
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-14 11:12:33 +0900

    Added a bottle.

---
+++ Assets/Scripts/BottleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottleController : MonoBehaviour {
+    public float squashTime = 0.4f;
+
+    float squash;
+
+	void Update () {
+        if (Input.GetMouseButton (0)) {
+            squash = Mathf.Min (squash + Time.deltaTime * 100.0f / squashTime, 100.0f);
+        } else {
+            squash = Mathf.Max (squash - Time.deltaTime * 100.0f / squashTime, 0);
+        }
+
+        GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, squash);
+        GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (1, squash);
+        GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (2, squash);
+    }
+}
