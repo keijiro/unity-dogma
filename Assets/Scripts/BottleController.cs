fabfcbfc7e4b3c53cd56c9fb42f4aaf102d8893c
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-16 14:09:45 +0900

    Added LeapMotion controller.

--- Assets/Scripts/BottleController.cs
+++ Assets/Scripts/BottleController.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BottleController : MonoBehaviour {
+public class BottleController : MonoBehaviour
+{
     public float squashTime = 0.4f;
-
+    Leap.Controller leap;
     float squash;
 
-	void Update () {
-        if (Input.GetMouseButton (0)) {
-            squash = Mathf.Min (squash + Time.deltaTime * 100.0f / squashTime, 100.0f);
+    void Awake ()
+    {
+        leap = new Leap.Controller ();
+    }
+
+    void Update ()
+    {
+        var frame = leap.Frame ();
+        var openness = 0.0f;
+
+        if (frame.Hands.Count < 1) {
+            openness = 100.0f;
         } else {
-            squash = Mathf.Max (squash - Time.deltaTime * 100.0f / squashTime, 0);
+            var palmPosition = frame.Hands [0].PalmPosition;
+            foreach (var finger in frame.Hands[0].Fingers) {
+                var distance = (finger.TipPosition - palmPosition).Magnitude;
+                openness += distance * 0.4f;
+            }
+            openness = Mathf.Clamp (openness - 50.0f, 0.0f, 100.0f);
         }
+
+        Debug.Log (openness);
+
+        squash = 100.0f - openness;
 
         GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (0, squash);
         GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (1, squash);
