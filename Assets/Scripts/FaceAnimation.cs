cffc6c03610d19414e80c7366ee9e5d62ad5b88b
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-16 16:07:04 +0900

    Added face animation.

---
+++ Assets/Scripts/FaceAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceAnimation : MonoBehaviour
+{
+    float shake;
+    float smile;
+
+    void Update ()
+    {
+        Vector3 waving = new Vector3 (
+            Mathf.Sin (Time.time * 2.621f),
+            Mathf.Sin (Time.time * 2.973f),
+            Mathf.Sin (Time.time * 1.789f)
+        );
+
+        transform.localPosition = waving * 0.1f + Random.onUnitSphere * shake;
+
+        GetComponent<SkinnedMeshRenderer> ().SetBlendShapeWeight (1, Mathf.Min (smile * 2000.0f, 100.0f));
+
+        shake *= Mathf.Exp (-10.0f * Time.deltaTime);
+        smile = Mathf.Max (0.0f, smile - Time.deltaTime);
+    }
+
+    public void BeginAnimation(string mode) {
+        if (mode == "anger") {
+            shake = 0.3f;
+        } else if (mode == "smile") {
+            smile = 0.6f;
+        }
+    }
+}
