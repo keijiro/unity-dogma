922ff71b31e21a0c5a48b097a80720308418fc5a
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-16 19:31:50 +0900

    Added Stimulator class.

---
+++ Assets/Scripts/Stimulator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stimulator : MonoBehaviour
+{
+    public float score = 1.0f;
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag == "Face") {
+            collision.gameObject.SendMessage("BeginAnimation", score > 0.0f ? "smile" : "anger");
+        }
+    }
+}
