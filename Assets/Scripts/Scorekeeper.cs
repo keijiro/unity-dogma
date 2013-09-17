fa44ec19718afd76594d4100340dc5b394f22f22
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-17 17:56:51 +0900

    Scorekeeper.

---
+++ Assets/Scripts/Scorekeeper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scorekeeper : MonoBehaviour
+{
+    public GUIStyle labelStyle;
+    int score;
+
+    public int AddSubScore (int delta)
+    {
+        score += delta;
+        return score;
+    }
+
+    void OnGUI ()
+    {
+        GUI.Label (new Rect (32, 32, Screen.width - 64, Screen.height - 64), "しあわせ : " + score, labelStyle);
+    }
+}
--- Assets/Scripts/Stimulator.cs
+++ Assets/Scripts/Stimulator.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class Stimulator : MonoBehaviour
 {
-    public float score = 1.0f;
+    static Scorekeeper scorekeeperReference;
+    public int score;
+
+    Scorekeeper GetScorekeeper() {
+        if (scorekeeperReference == null) {
+            scorekeeperReference = FindObjectOfType<Scorekeeper>();
+        }
+        return scorekeeperReference;
+    }
 
     void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.tag == "Face") {
-            collision.gameObject.SendMessage("BeginAnimation", score > 0.0f ? "smile" : "anger");
+            collision.gameObject.SendMessage("BeginAnimation", score > 0 ? "smile" : "anger");
+            GetScorekeeper().AddSubScore(score);
         }
     }
 }
