f2cdb4efa4b11a9d2681b079b2d6c0db72260b49
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-09-18 16:08:37 +0900

    Added some intermediate scenes.

--- Assets/Scripts/Stimulator.cs
+++ Assets/Scripts/Stimulator.cs
@@ -6,18 +6,16 @@ public class Stimulator : MonoBehaviour
     static Scorekeeper scorekeeperReference;
     public int score;
 
-    Scorekeeper GetScorekeeper() {
-        if (scorekeeperReference == null) {
-            scorekeeperReference = FindObjectOfType<Scorekeeper>();
-        }
-        return scorekeeperReference;
-    }
-
     void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.tag == "Face") {
             collision.gameObject.SendMessage("BeginAnimation", score > 0 ? "smile" : "anger");
-            GetScorekeeper().AddSubScore(score);
+            if (scorekeeperReference == null) {
+                scorekeeperReference = FindObjectOfType<Scorekeeper>();
+            }
+            if (scorekeeperReference != null) {
+                scorekeeperReference.AddSubScore(score);
+            }
         }
     }
 }
